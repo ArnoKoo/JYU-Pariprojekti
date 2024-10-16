98641cba184e45406e947e407d89b06dd6ac0f75
Daniel Lobko <stfnlbk@gmail.com>
2024-10-16 16:19:58 +0300

    Lisäsin Tank Clash -projektin

---
+++ Tank Clash/Tank Clash.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+/// @author Stefan
+/// @version 07.10.2024
+/// <summary>
+/// 
+/// </summary>
+public class Tank_Clash
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static void Main()
+    {
+        Console.WriteLine("Welcome to the Tank Clash!");
+    }   
+
+}
