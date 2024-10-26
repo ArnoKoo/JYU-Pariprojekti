f22e74c20164b66080ebd48f36bb4f732cf0f385
Daniel Lobko <stfnlbk@gmail.com>
2024-10-26 23:26:23 +0300

    Muutoksia
    Lisäsin muutaman funktion, kuten:
    Nyt tankit luodaan erikseen funktiolla
    Luodeilla on cooldown
    Luodit katoaa kun ne osuu johonkin
    Lisäsin rajat

--- Tank Clash/Tank Clash.cs
+++ Tank Clash/Tank Clash.cs
@@ -8,61 +8,74 @@ using Jypeli;
 /// </summary>
 public class Tank_Clash : PhysicsGame
 {
-    public PhysicsObject pelaaja1;
-    public PhysicsObject pelaaja2;
 
     public override void Begin() //Begin() on vissiin Update - Arska
     {
         LuoKentta();
-        OhjainLogiikka();
+        PhysicsObject tankki1 = LuoTankki(this, -200, 0, Color.Green, 2);
+        PhysicsObject tankki2 = LuoTankki(this, 200, 0, Color.Red, -2);
+        OhjainLogiikka(tankki1, tankki2);
     }
 
-    void OhjainLogiikka()
+    void OhjainLogiikka(PhysicsObject pelaaja1, PhysicsObject pelaaja2) 
     {
         //Pelaaja1
         Keyboard.Listen(Key.W, ButtonState.Down, Eteen, "P1 ylös", pelaaja1); //ylös
         Keyboard.Listen(Key.S, ButtonState.Down, Taakse, "P1 alas", pelaaja1); //alas
 
-        Keyboard.Listen(Key.A, ButtonState.Down, TankkienRotaasi, "P1 vasen", pelaaja1, -5.0); //vasen
+        Keyboard.Listen(Key.A, ButtonState.Down, TankkienRotaasi, "P1 vasen", pelaaja1, 5.0); //vasen
         Keyboard.Listen(Key.D, ButtonState.Down, TankkienRotaasi, "P1 vasen", pelaaja1, -5.0); //oikea
         
-        Keyboard.Listen(Key.F, ButtonState.Pressed, PiuPiu, "P1 ammu", pelaaja1); //ampuminen
+        Keyboard.Listen(Key.F, ButtonState.Pressed, CoolDown, "P1 ammu", pelaaja1); //ampuminen
         
-        //Pelaaja2
+        //Pelaaja2 
         Keyboard.Listen(Key.Up, ButtonState.Down, Eteen, "P2 ylös", pelaaja2); //ylös
         Keyboard.Listen(Key.Down, ButtonState.Down, Taakse, "P2 alas", pelaaja2); //alas
         
-        Keyboard.Listen(Key.Left, ButtonState.Down, TankkienRotaasi, "P2 vasen", pelaaja2, -5.0); //vasen
+        Keyboard.Listen(Key.Left, ButtonState.Down, TankkienRotaasi, "P2 vasen", pelaaja2, 5.0); //vasen
         Keyboard.Listen(Key.Right, ButtonState.Down, TankkienRotaasi, "P2 vasen", pelaaja2, -5.0); //oikea
         
-        Keyboard.Listen(Key.RightControl, ButtonState.Pressed, PiuPiu, "P2 ammu", pelaaja2); //ampuminen
+        Keyboard.Listen(Key.RightControl, ButtonState.Pressed, CoolDown, "P2 ammu", pelaaja2); //ampuminen
     }
-    
-    void LuoKentta() //Teoriassa vois luoda erikseen tankit mutta ei ole budjettia (aka jaksamista) moiselle - Arska
+
+    public static PhysicsObject LuoTankki(PhysicsGame peli, double x, double y, Color väri, int paikka) // Funkio joka luo tankkeja, tälle tulee tarvetta, kun aletaan tekee koolisiota
     {
-        //P1
-        pelaaja1 = new PhysicsObject(40, 40);
-        pelaaja1.Shape = Shape.Rectangle;
-        pelaaja1.Color = Color.Green;
-        pelaaja1.X = -200; //Spawnipointti. Palataan tähän kohtaan silloin, kun tehdään respawn logiikkaa - Arska
-        pelaaja1.LinearDamping = 0.95;
-        pelaaja1.MaxVelocity = 300;
-        pelaaja1.Restitution = 0.2; 
-        Add(pelaaja1);
+        PhysicsObject tankki = new PhysicsObject(40, 40, Shape.Rectangle);
+        tankki.Color = väri;
+        tankki.X = x;
+        tankki.Y = y;
+        tankki.LinearDamping = 0.50;
+        tankki.MaxVelocity = 100;
+        tankki.Restitution = 0.0;
+        peli.Add(tankki);
 
-        //P2
-        pelaaja2 = new PhysicsObject(40, 40);
-        pelaaja2.Shape = Shape.Rectangle;
-        pelaaja2.Color = Color.Red;
-        pelaaja2.X = 200; //Spawnipointti. Palataan tähän kohtaan silloin, kun tehdään respawn logiikkaa - Arska
-        pelaaja2.LinearDamping = 0.95;
-        pelaaja2.MaxVelocity = 300;
-        pelaaja2.Restitution = 0.2; 
-        Add(pelaaja2);
-
+        return tankki;
+    }
+    private void LuoKentta() //Teoriassa vois luoda erikseen tankit mutta ei ole budjettia (aka jaksamista) moiselle - Arska  // on aikaa ja jaksamista
+    {
         //Rajat
         Level.CreateBorders(1.0, false);
         Camera.ZoomToLevel();
+        
+        PhysicsObject vasenReuna = Level.CreateLeftBorder();
+        vasenReuna.Restitution = 1.0;
+        vasenReuna.KineticFriction = 0.0;
+        vasenReuna.IsVisible = false;
+
+        PhysicsObject oikeaReuna = Level.CreateRightBorder();
+        oikeaReuna.Restitution = 1.0;
+        oikeaReuna.KineticFriction = 0.0;
+        oikeaReuna.IsVisible = false;
+
+        PhysicsObject ylaReuna = Level.CreateTopBorder();
+        ylaReuna.Restitution = 1.0;
+        ylaReuna.KineticFriction = 0.0;
+        ylaReuna.IsVisible = false;
+
+        PhysicsObject alaReuna = Level.CreateBottomBorder();
+        alaReuna.Restitution = 1.0;
+        alaReuna.KineticFriction = 0.0;
+        alaReuna.IsVisible = false;
     }
 
     void Eteen(PhysicsObject pelaaja) //pelaajat liikkuu eteenpäin
@@ -82,6 +95,21 @@ public class Tank_Clash : PhysicsGame
         pelaaja.Angle += Angle.FromDegrees(asteenMuutokset);
     }
 
+    private const double CooldownDuration = 0.5;
+    private bool isOnCooldown = false;
+
+    void CoolDown(PhysicsObject pelaaja)
+    {
+        if (isOnCooldown)
+        {
+            return; 
+        }
+
+        PiuPiu(pelaaja);
+        isOnCooldown = true;
+
+        Timer.SingleShot(CooldownDuration, () => isOnCooldown = false);
+    }
     void PiuPiu(PhysicsObject pelaaja)
     {
         PhysicsObject projektiili = new PhysicsObject(10, 10);
@@ -91,10 +119,22 @@ public class Tank_Clash : PhysicsGame
         
         Vector ampumaSuunta = Vector.FromLengthAndAngle(500.0, pelaaja.Angle);
         projektiili.Velocity = ampumaSuunta;
-        projektiili.Restitution = 0.3; //pomppimista varten
+        projektiili.Restitution = 0; //pomppimista varten
         Add(projektiili);
+
+        projektiili.Tag = pelaaja;
+
+        AddCollisionHandler(projektiili, AmmusOsui);
+
     }
-}
+
+    void AmmusOsui(PhysicsObject projektiili, PhysicsObject kohde) // Jos luoti osuu johonkin
+    {
+        if (kohde != projektiili.Tag)
+        {       
+            Remove(projektiili);
+        }
+    }
 
 public class Program
 {
