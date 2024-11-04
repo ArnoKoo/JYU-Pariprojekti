21a9e35e82df43daaefc836f3d5121869535b084
ArnoKoo <arno.korhonen05@gmail.com>
2024-11-04 15:46:40 +0200

    Audio, spritejä ja uudelleenkirjotusta
    Audio ampumiselle ja osumiselle, spritejä ja uudelleenkirjotusta

--- Tank Clash/Tank Clash.cs
+++ Tank Clash/Tank Clash.cs
@@ -8,18 +8,57 @@ using Jypeli;
 /// </summary>
 public class Tank_Clash : PhysicsGame
 {
+    private IntMeter pelaaja1HP;
+    private IntMeter pelaaja2HP;
 
+    private Image pelaaja1Kuva;
+    private Image pelaaja2Kuva;
+    private SoundEffect ampuminen;
+    private SoundEffect osuminen;
     public override void Begin() //Begin() on vissiin Update - Arska
     {
+        pelaaja1Kuva = LoadImage("P1Tank.png");
+        pelaaja2Kuva = LoadImage("P2Tank.png");
+        ampuminen = LoadSoundEffect("pew-pew-lame-sound-effect.wav");
+        osuminen = LoadSoundEffect("homemadeoof-47509.wav");
+
         LuoKentta();
+
         PhysicsObject tankki1 = LuoTankki(this, -200, 0, Color.Green, 2);
+        tankki1.Image = pelaaja1Kuva;
+        tankki1.Size = new Vector(128, 128); // Set size based on image
+
         PhysicsObject tankki2 = LuoTankki(this, 200, 0, Color.Red, -2);
-        LuoPistelaskuri(-400);
-        LuoPistelaskuri(400);
+        tankki2.Image = pelaaja2Kuva;
+        tankki2.Size = new Vector(128, 128); // Set size based on image
+
+        pelaaja1HP = new IntMeter(5);
+        pelaaja2HP = new IntMeter(5);
+
+        LuoPistelaskuri(-400, pelaaja1HP);
+        LuoPistelaskuri(400, pelaaja2HP);
+
+        tankki1.Tag = "Pelaaja1";
+        tankki2.Tag = "Pelaaja2";
+
+        tankki1.KineticFriction = 0.5;
+        tankki1.StaticFriction = 0.5;
+        tankki1.LinearDamping = 0.5;
+        tankki1.AngularDamping = 1.0;
+
+        tankki2.KineticFriction = 0.5;
+        tankki2.StaticFriction = 0.5;
+        tankki2.LinearDamping = 0.5;
+        tankki2.AngularDamping = 1.0;
+
+        Console.WriteLine($"Pelaaja 1 Image Size: {pelaaja1Kuva.Width}x{pelaaja1Kuva.Height}");
+        Console.WriteLine($"Tank 1 Physics Size: {tankki1.Size.X}x{tankki1.Size.Y}");
+        Console.WriteLine($"Pelaaja 2 Image Size: {pelaaja2Kuva.Width}x{pelaaja2Kuva.Height}");
+        Console.WriteLine($"Tank 2 Physics Size: {tankki2.Size.X}x{tankki2.Size.Y}");
         OhjainLogiikka(tankki1, tankki2);
     }
 
-    void OhjainLogiikka(PhysicsObject pelaaja1, PhysicsObject pelaaja2) 
+    void OhjainLogiikka(PhysicsObject pelaaja1, PhysicsObject pelaaja2)
     {
         //Pelaaja1
         Keyboard.Listen(Key.W, ButtonState.Down, Eteen, "P1 ylös", pelaaja1); //ylös
@@ -27,38 +66,42 @@ public class Tank_Clash : PhysicsGame
 
         Keyboard.Listen(Key.A, ButtonState.Down, TankkienRotaasi, "P1 vasen", pelaaja1, 5.0); //vasen
         Keyboard.Listen(Key.D, ButtonState.Down, TankkienRotaasi, "P1 vasen", pelaaja1, -5.0); //oikea
-        
+
         Keyboard.Listen(Key.F, ButtonState.Pressed, CoolDown, "P1 ammu", pelaaja1); //ampuminen
-        
+
         //Pelaaja2 
         Keyboard.Listen(Key.Up, ButtonState.Down, Eteen, "P2 ylös", pelaaja2); //ylös
         Keyboard.Listen(Key.Down, ButtonState.Down, Taakse, "P2 alas", pelaaja2); //alas
-        
+
         Keyboard.Listen(Key.Left, ButtonState.Down, TankkienRotaasi, "P2 vasen", pelaaja2, 5.0); //vasen
         Keyboard.Listen(Key.Right, ButtonState.Down, TankkienRotaasi, "P2 vasen", pelaaja2, -5.0); //oikea
-        
+
         Keyboard.Listen(Key.RightControl, ButtonState.Pressed, CoolDown, "P2 ammu", pelaaja2); //ampuminen
     }
 
-    public static PhysicsObject LuoTankki(PhysicsGame peli, double x, double y, Color väri, int paikka) // Funkio joka luo tankkeja, tälle tulee tarvetta, kun aletaan tekee koolisiota
+    public static PhysicsObject LuoTankki(PhysicsGame peli, double x, double y, Color väri, int paikka) // Funkio joka luo tankkeja, tälle tulee tarvetta, kun aletaan tekee koolisiota //skaalauksesta tuli täs hyödytön, ku skaalaan ne nyt kuvan mukaan -Arska
     {
-        PhysicsObject tankki = new PhysicsObject(40, 40, Shape.Rectangle);
+        PhysicsObject tankki = new PhysicsObject(1, 1, Shape.Rectangle);
         tankki.Color = väri;
         tankki.X = x;
         tankki.Y = y;
+        
+        tankki.Angle = Angle.FromDegrees(90);
+        
         tankki.LinearDamping = 0.50;
         tankki.MaxVelocity = 100;
         tankki.Restitution = 0.0;
         peli.Add(tankki);
-
+        
         return tankki;
     }
+
     private void LuoKentta() //Teoriassa vois luoda erikseen tankit mutta ei ole budjettia (aka jaksamista) moiselle - Arska  // on aikaa ja jaksamista
     {
         //Rajat
         Level.CreateBorders(1.0, false);
         Camera.ZoomToLevel();
-        
+
         PhysicsObject vasenReuna = Level.CreateLeftBorder();
         vasenReuna.Restitution = 1.0;
         vasenReuna.KineticFriction = 0.0;
@@ -82,13 +125,13 @@ public class Tank_Clash : PhysicsGame
 
     void Eteen(PhysicsObject pelaaja) //pelaajat liikkuu eteenpäin
     {
-        Vector suunta = Vector.FromLengthAndAngle(500.0, pelaaja.Angle);
+        Vector suunta = Vector.FromLengthAndAngle(1000.0, pelaaja.Angle);
         pelaaja.Push(suunta);
     }
-    
+
     void Taakse(PhysicsObject pelaaja) //pelaajat liikkuu taakse
     {
-        Vector suunta = Vector.FromLengthAndAngle(-500.0, pelaaja.Angle);
+        Vector suunta = Vector.FromLengthAndAngle(-1000.0, pelaaja.Angle);
         pelaaja.Push(suunta);
     }
 
@@ -104,7 +147,7 @@ public class Tank_Clash : PhysicsGame
     {
         if (isOnCooldown)
         {
-            return; 
+            return;
         }
 
         PiuPiu(pelaaja);
@@ -112,41 +155,48 @@ public class Tank_Clash : PhysicsGame
 
         Timer.SingleShot(CooldownDuration, () => isOnCooldown = false);
     }
+
     void PiuPiu(PhysicsObject pelaaja)
     {
         PhysicsObject projektiili = new PhysicsObject(10, 10);
         projektiili.Shape = Shape.Circle;
         projektiili.Color = Color.Yellow;
         projektiili.Position = pelaaja.Position;
-        
+
         Vector ampumaSuunta = Vector.FromLengthAndAngle(500.0, pelaaja.Angle);
         projektiili.Velocity = ampumaSuunta;
         projektiili.Restitution = 0; //pomppimista varten
         Add(projektiili);
 
         projektiili.Tag = pelaaja;
-
         AddCollisionHandler(projektiili, AmmusOsui);
 
+        ampuminen.Play();
     }
 
     void AmmusOsui(PhysicsObject projektiili, PhysicsObject kohde) // Jos luoti osuu johonkin
     {
-        if (kohde != projektiili.Tag)
-        {       
-            Remove(projektiili);
+        if (kohde != projektiili.Tag) //olit oikeilla jäljillä, kiitos headstartist Dani - Arska
+        {
+            if (kohde.Tag == "Pelaaja1") //Jos osuu pelaajaan 1...
+            {
+                pelaaja1HP.Value--;
+            }
+
+            else if (kohde.Tag == "Pelaaja2") //Jos osuu pelaajaan 2...
+            {
+                pelaaja2HP.Value--;
+            }
+            osuminen.Play();
+            Remove(projektiili); //huolimatta mitä käy, poistetaan projektiili
         }
     }
-    
-    void LuoPistelaskuri(double x)
+
+    void LuoPistelaskuri(double x, IntMeter pistelaskuri) //rewritasin tän - Arska
     {
-        IntMeter pistelaskuri;
-
-        pistelaskuri = new IntMeter(3);
-
         Label pistenaytto = new Label();
         pistenaytto.Title = "HP: ";
-        pistenaytto.X =  x;
+        pistenaytto.X = x;
         pistenaytto.Y = Screen.Top - 100;
         pistenaytto.TextColor = Color.Black;
         pistenaytto.Color = Color.White;
@@ -154,11 +204,12 @@ public class Tank_Clash : PhysicsGame
         pistenaytto.BindTo(pistelaskuri);
         Add(pistenaytto);
     }
+}
 
 public class Program
-{
+{ 
     public static void Main()
-    {
+    { 
         new Tank_Clash().Run();
     }
 }
--- Tank Clash/obj/Debug/net8.0/Tank Clash.AssemblyInfo.cs
+++ Tank Clash/obj/Debug/net8.0/Tank Clash.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tank Clash")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdc910708f70d45a1fdf307c60a2085fbc1f670d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95315f95b063dbc9942cdd800b988b2fa5178702")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tank Clash")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tank Clash")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
